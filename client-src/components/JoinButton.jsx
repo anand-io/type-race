7aea572b2043205f35082a7ac49df71bf6774cbb
Anand <anand.seeni@a-cti.com>
2016-10-14 00:44:28 +0530

    showing race again when race is over

--- client-src/components/JoinButton.jsx
+++ client-src/components/JoinButton.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { joinRace } from '../actions';
 
-let Start = ({ dispatch, show }) => (
+let Start = ({ dispatch, show, showRaceResult }) => (
   <a
     style={{ display: show ? 'block' : 'none' }}
     onClick={() => {
       dispatch(joinRace(location.pathname.replace('/', '')));
     }}
-  >Join Race</a>
+  >{showRaceResult ? "Race Again" : "Join Race"}</a>
 );
 
 const mapStateToProps = (state) => ({
   show: !state.joinedRace,
+  showRaceResult: state.showRaceResult
 })
 
 Start = connect(mapStateToProps)(Start);
