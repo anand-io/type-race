f221771990c0fc3dba0c18374a0b9c08a23dddb8
parthibhan kp <parthibhank82@gmail.com>
2016-10-13 15:08:04 +0530

    fixed changing word red

--- client-src/components/Paragraph.jsx
+++ client-src/components/Paragraph.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-function getStyle(typingWordIndex, index) {
-  if (index === typingWordIndex)
-    return {
-      color: 'cornflowerblue',
-      fontSize: '25px',
-      textDecoration: 'underline'
-    }
-  else
-    return {}
+const getStyle = (typingWordIndex, index, wrongWord) => {
+  if (index !== typingWordIndex) return {}
+  return {
+    color: wrongWord ? 'RED' : 'cornflowerblue',
+    fontSize: '25px',
+    textDecoration: 'underline'
+  }
 }
 
-let Paragraph = ({ paragraph, show, typingWordIndex }) => (
+let Paragraph = ({ paragraph, show, typingWordIndex, wrongWord }) => (
   <div style={{ display: show ? 'block' : 'none', width:'300px' }}>
     {Array.isArray(paragraph.words) ?
       paragraph.words.map((word, index) =>
         <span
         key={index}
-        style={getStyle(typingWordIndex, index)} >
+        style={getStyle(typingWordIndex, index, wrongWord)} >
           {index == paragraph.length - 1 ? word : `${word} `}
         </span>) : ''}
   </div>
@@ -28,6 +26,7 @@ const mapStateToProps = (state) => ({
   paragraph: state.paragraph,
   show: state.everyoneReady,
   typingWordIndex: state.typingWordIndex,
+  wrongWord: state.wrongWord
 })
 
 Paragraph = connect(
