7ccc4e4240301663d47100124db08751226c7632
Anand <anand.seeni@a-cti.com>
2016-10-14 01:27:16 +0530

    fixed timer formate

--- client-src/components/RaceTimer.jsx
+++ client-src/components/RaceTimer.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-let RaceTimer = ({ show, seconds }) => (
-  <div
-    style={{ display: show ? 'block' : 'none' }}
-  >
-    {`00:${seconds}`}
-  </div>
-)
+let RaceTimer = ({ show, seconds }) => {
+  function pad(val) {
+    return val > 9 ? val : `0${val}`;
+  }
+  const min = parseInt(seconds / 60, 10) % 60;
+  return (
+    <div
+      style={{ display: show ? 'block' : 'none' }}
+    >
+      {`${pad(min)}:${pad(seconds % 60)}`}
+    </div>
+  );
+}
 
 const mapStateToProps = (state) => ({
   show: state.raceStarted && !state.finishRace,
