5ae0d917f49182be9c9962cb8c8753feec6e7a0f
Anand <anand.seeni@a-cti.com>
2016-10-14 19:42:54 +0530

    changed the primus ws connect url

--- client-src/services/WebSocketService.js
+++ client-src/services/WebSocketService.js
@@ -5,7 +5,7 @@ function WebSocketService() {}
 // Add some event emmiter.
 WebSocketService.prototype.init = function init(onParticipantJoined, onStartCounter,
   onParticipantCount, onRaceOver) {
-  this.primus = Primus.connect(`http://localhost:3000/?myId=${myId}`);
+  this.primus = Primus.connect(`${location.protocol}//${location.host}/?myId=${myId}`);
   this.onParticipantJoined = onParticipantJoined;
   this.onStartCounter = onStartCounter;
   this.onParticipantCount = onParticipantCount;
