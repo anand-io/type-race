cae4244877f406bbf6b5d6edee3b5ef85526e13f
Anand <anand.seeni@a-cti.com>
2016-10-14 09:52:27 +0530

    styles for waiting message and timer

--- client-src/components/Waiting.jsx
+++ client-src/components/Waiting.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 let Waiting = ({ dispatch, show }) => (
-  <p
-    style={{ display: show ? 'block' : 'none' }}
-  >Waiting for others to join.</p>
+  <div className='waiting-overlay'>
+    <p
+      style={{ display: show ? 'block' : 'none' }}
+    >
+    Waiting for others to join.
+    </p>
+  </div>
 );
 
 const mapStateToProps = (state) => ({
