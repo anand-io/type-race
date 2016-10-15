c7fb3d667ee9f0248d5fdaca0fee633bf036fe12
Anand <anand.seeni@a-cti.com>
2016-10-15 08:02:40 +0530

    showing waiting for others to join untill when only one person has joined

--- client-src/components/Participants.jsx
+++ client-src/components/Participants.jsx
@@ -1,28 +1,39 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-let Participants = ({ participants, paragraph, finishedRace, myId }) => (
-  <ul className="challenge-user">
-    {participants.map(participant => {
-      const percentageCompleted = (participant.noOfCharacters / paragraph.raw.length) * 100;
-      const participantId = participant.id === myId ?
-      'You' : participant.id.split('-')[0];
-      return (
-        <li
-          style={{ display: finishedRace && participant.isFinished ? 'none' : 'block' }}
-          className="skill"
-          key={participant.id}
-        >
-          <div
-            className="overlay"
-            style={{ width: `${percentageCompleted}%`}}
-          />
-          <span>{`${participantId}: ${Math.ceil(participant.wpm)} WPM`}</span>
-        </li>
-      );
-    })}
-  </ul>
-)
+let Participants = ({ participants, paragraph, finishedRace, myId }) => {
+  const rParticipants = [...participants];
+  if (rParticipants.length === 1) {
+    rParticipants.push({ waiting: true, id: 'Waiting for others to join...'});
+  }
+  return (
+    <ul className="challenge-user">
+      {rParticipants.map(participant => {
+        let percentageCompleted = (participant.noOfCharacters / paragraph.raw.length) * 100;
+        const participantId = participant.id === myId ?
+        'You' : participant.id.split('-')[0];
+        let WPM = `: ${Math.ceil(participant.wpm)} WPM`;
+        if (participant.waiting) {
+          WPM = '';
+          percentageCompleted = 0;
+        }
+        return (
+          <li
+            style={{ display: finishedRace && participant.isFinished ? 'none' : 'block' }}
+            className="skill"
+            key={participant.id}
+          >
+            <div
+              className="overlay"
+              style={{ width: `${percentageCompleted}%`}}
+            />
+          <span>{`${participantId}${WPM}`}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 
 const mapStateToProps = (state) => ({
   participants: state.participants,
