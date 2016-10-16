f28327e7b372c9b92ffecd050267fc56eea75d8e
parthibhan kp <parthibhank82@gmail.com>
2016-10-17 00:48:54 +0530

    get a new token everytime

--- routes/index.js
+++ routes/index.js
@@ -62,6 +62,30 @@ router.get('/getTokens', function(req, res, next) {
     })
 });
 
+function getNewAccessToken(refresh_token, callback) {
+  request.post({
+    url: 'https://access.anywhereworks.com/o/oauth2/v1/token',
+    form: { refresh_token: refresh_token, client_id: credentials.client_id,
+            client_secret: credentials.client_secret,
+            grant_type: 'refresh_token'
+          },
+    headers: {
+      'Content-Type' : 'application/x-www-form-urlencoded'
+    },
+  },
+  function(e, r, body) {
+    console.log(e);
+    console.log('respose : ');
+    console.log(body)
+    body = JSON.parse(body);
+    console.log(body)
+    if (body && body.access_token) {
+      console.log(`got new access_token .... ${body.access_token}`);
+      callback(body.access_token);
+    }
+  });
+}
+
 router.post('/feed', function(req, res, next) {
   console.log('inside feed')
   var user_id = req.body.user_id;
@@ -70,19 +94,21 @@ router.post('/feed', function(req, res, next) {
     userService.getTokens(user_id, function(model) {
       console.log(`posting feed : ${user_id} ${content}`);
       if (model.access_token && model.refresh_token) {
-        var options = {
-          url: 'https://api.anywhereworks.com/api/v1/feed',
-          body: JSON.stringify({
-	           "content": content,
-             "type" : "update"
-          }),
-          headers: {
-            'Content-Type' : 'application/json',
-            'Authorization': `Bearer ${model.access_token}`
+        getNewAccessToken(model.refresh_token, function(access_token) {
+          var options = {
+            url: 'https://api.anywhereworks.com/api/v1/feed',
+            body: JSON.stringify({
+  	           "content": content,
+               "type" : "update"
+            }),
+            headers: {
+              'Content-Type' : 'application/json',
+              'Authorization': `Bearer ${access_token}`
+            }
           }
-        }
-        request.post(options, function(e, r, body) {
-          console.log(body);
+          request.post(options, function(e, r, body) {
+            console.log(body);
+          });
         });
       }
     });
