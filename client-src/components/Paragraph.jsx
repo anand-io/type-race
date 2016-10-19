7d25d8495fc4e7883acf9701563cef9980938239
Anand <anand.seeni@a-cti.com>
2016-10-19 19:01:10 +0530

    changed paragraphs and few css properties

--- client-src/components/Paragraph.jsx
+++ client-src/components/Paragraph.jsx
@@ -1,41 +1,51 @@
-import React from 'react'
+import React, { Component }from 'react'
 import { connect } from 'react-redux'
 
-const getStyle = (typingWordIndex, index, wrongWord) => {
-  if (index !== typingWordIndex) return {}
-  return {
-    color: wrongWord ? 'RED' : 'cornflowerblue',
-    fontSize: '19px',
-  }
-}
 
-let Paragraph = ({ paragraph, show, typingWordIndex, wrongWord, placeholder, participantsHeight }) => {
-  const height = 430 - 120 - participantsHeight;
-  return (
-    <div
-      className='paragraph'
-      style={{ display: show ? 'block' : 'none', height }}
-    >
-      {
-        !placeholder && paragraph.words ?
-        paragraph.words.map((word, index) =>
-          <span
-          key={index}
-          style={getStyle(typingWordIndex, index, wrongWord)} >
-            {index == paragraph.length - 1 ? word : `${word} `}
-          </span>)
-          :
-        <div className="temp_item">
-            <div className="temp_line animated-background" />
-            <div className="temp_line animated-background" />
-            <div className="temp_line animated-background" />
-            <div className="temp_line animated-background" />
-            <div className="temp_line animated-background" />
-            <div className="temp_line animated-background" />
-        </div>
-      }
-    </div>
-  );
+class Paragraph extends Component {
+  componentDidUpdate() {
+    if (this.currentWordNode) this.currentWordNode.scrollIntoView();
+  }
+
+  render() {
+    const { paragraph, show, typingWordIndex, wrongWord, placeholder, participantsHeight, noOfCharactersTyped } = this.props;
+    const height = 430 - 120 - participantsHeight;
+    let [firstPart, currentWord, lastPart] = ['', '', ''];
+    if (paragraph.raw) {
+      firstPart = paragraph.raw.substring(0, noOfCharactersTyped);
+      currentWord = paragraph.words[typingWordIndex];
+      const currentWordEndIndex = noOfCharactersTyped + paragraph.words[typingWordIndex].length;
+      lastPart = paragraph.raw.substring(currentWordEndIndex, paragraph.raw.length);
+    }
+    const currentWordStyle = {
+      color: wrongWord ? '#DE2626' : '#83C447',
+      textDecoration: 'underline',
+    };
+    return (
+      <div
+        className='paragraph'
+        style={{ display: show ? 'block' : 'none', height }}
+      >
+        {
+          !placeholder && paragraph.words ?
+          <span>
+            <span>{firstPart}</span>
+            <span style={currentWordStyle} ref={node => { this.currentWordNode = node; }}>{currentWord}</span>
+            <span>{lastPart}</span>
+          </span>
+            :
+          <div className="temp_item">
+              <div className="temp_line animated-background" />
+              <div className="temp_line animated-background" />
+              <div className="temp_line animated-background" />
+              <div className="temp_line animated-background" />
+              <div className="temp_line animated-background" />
+              <div className="temp_line animated-background" />
+          </div>
+        }
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
@@ -45,6 +55,7 @@ const mapStateToProps = (state) => ({
   wrongWord: state.wrongWord,
   placeholder: state.participants.length === 1 && !state.isPractice,
   participantsHeight: state.participantsHeight,
+  noOfCharactersTyped: state.noOfCharactersTyped,
 })
 
 Paragraph = connect(
